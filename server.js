60315c7049eebb94716becbf88a5e751a23f87d9
apisit somtapthai <apisitjadi@gmail.com>
2020-03-15 22:19:21 +0700

    add .gitignore file

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  //res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  //res.end('Hello World!');
+
+  request('https://www.google.com', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }
+  });
+
 }).listen(8080);
